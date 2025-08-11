c2bcbafd99989d31a44d95dd6d733ce78f4f05cd
Yousef-jaber4 <yousefjaber437@gmail.com>
2025-08-11 23:35:08 +0300

    Login and Home Page

---
+++ HomeJs.js
@@ -0,0 +1,44 @@
+const nameUser = window.localStorage.getItem('name');
+const token = localStorage.getItem("JWT-Token");
+let balance = localStorage.getItem('balance');
+
+let nameElement = document.getElementById('name');
+if (nameUser) {
+    nameElement.textContent = nameUser;
+}
+
+let balan = document.querySelector('.box-value');
+if (balan && balance) {
+    balan.textContent = balance + " $";
+}
+
+checkToken();
+
+function checkToken() {
+    if (token) {
+        const decoded = jwt_decode(token);
+        const now = Date.now() / 1000;
+
+        if (decoded.exp < now) {
+            localStorage.removeItem("JWT-Token");
+            window.location.href = "login.html";
+        }
+    } else {
+        window.location.href = "login.html";
+    }
+}
+
+function updateClock() {
+    const now = new Date();
+    let hrs = Number(String(now.getHours()).padStart(2, "0")) - 12;
+    let min = String(now.getMinutes()).padStart(2, "0");
+    let sec = String(now.getSeconds()).padStart(2, "0");
+
+
+    document.getElementById("hrs").textContent = hrs;
+    document.getElementById("min").textContent = min;
+    document.getElementById("sec").textContent = sec;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
